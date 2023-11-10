e790da8b1b9088ca4ed036477f14acc37aa0a34e
Ram-Ram007 <ramajayam2411@gmail.com>
2023-11-10 23:47:15 +0530

    modified

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const jokeContainer = document.getElementById('jokeContainer');
-const getJokeBtn = document.getElementById('getJokeBtn');
+const jokeContainer = document.getElementById("jokeContainer");
+const getJokeBtn = document.getElementById("getJokeBtn");
 
 const API_URL = `https://v2.jokeapi.dev/joke/Any?type=single`;
 
 getJoke();
 
+async function getJoke() {
+  const res = await fetch(API_URL);
 
-getJokeBtn.addEventListener('click', getJoke);
+  jokeContainer.innerHTML = data.joke;
+}
+
+getJokeBtn.addEventListener("click", getJoke);
